dc9e2832e7bb3cc78549996ea47815adefd43848
josecortez1 <jose.vazcortez@gmail.com>
2020-11-09 01:02:58 -0600

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div className="footer">
+                <a className="footer__a" href="https://josecortez1.github.io/">made by JoseCortez</a>
+            </div>
+        )
+    }
+}
