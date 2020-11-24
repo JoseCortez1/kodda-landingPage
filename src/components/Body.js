abe07377db9f45f81d5c9a0c74a2046c781a5fed
josecortez1 <jose.vazcortez@gmail.com>
2020-11-24 08:50:37 -0600

    new format of colors

--- src/components/Body.js
+++ src/components/Body.js
@@ -78,26 +78,39 @@ export default class Body extends Component {
 
                   <div className="main__data_inputs">
                         <label className="main__data_input__label" htmlFor="color">
-                            Colores
-                            <p> {"<Nombre> - <Fondo>"}</p>
+                            Colores ( Codigo )
                         </label>
                         <select name="color"  className="input__" id="color" value={this.state.color} onChange={this.handleChange}>
-                            <option value="DN">Dorado - Negro</option>
-                            <option value="VN">Verde - Negro</option>
-                            <option value="RN">Rojo - Negro</option>
-                            <option value="AN">Azul - Negro</option>
-                            <option value="ACN">Azul Cielo - Negro</option>
-                            <option value="AA">Amarillo - Azul</option>
-                            <option value="AD">Amarillo - Dorado</option>
-                            <option value="NB">Naranja - Blanco</option>
-                            <option value="RA">Rojo - Azul</option>
-                            <option value="VR">Verde - Rojo</option>
-                            <option value="AR">Amarillo - Rosa</option>
-                            <option value="TR">Turquesa - Rosa</option>
-                            <option value="DR">Dorado - Rosa</option>
-                            <option value="RB">Rojo - Blanco</option>
-                            <option value="RN">Rojo - Dorado</option>
-
+                            <option value="2001">2001</option>
+                            <option value="2002">2002</option>
+                            <option value="2003">2003</option>
+                            <option value="2004">2004</option>
+                            <option value="2005">2005</option>
+                            <option value="2006">2006</option>
+                            <option value="2007">2007</option>
+                            <option value="2008">2008</option>
+                            <option value="2009">2009</option>
+                            <option value="2010">2010</option>
+                            <option value="2011">2011</option>
+                            <option value="2012">2012</option>
+                            <option value="2013">2013</option>
+                            <option value="2014">2014</option>
+                            <option value="2015">2015</option>
+                            <option value="2016">2016</option>
+                            <option value="2017">2017</option>
+                            <option value="2018">2018</option>
+                            <option value="2019">2019</option>
+                            <option value="2020">2020</option>
+                            <option value="2021">2021</option>
+                            <option value="2022">2022</option>
+                            <option value="2023">2023</option>
+                            <option value="2024">2024</option>
+                            <option value="2025">2025</option>
+                            <option value="2026">2026</option>
+                            <option value="2027">2027</option>
+                            <option value="2028">2028</option>
+                            <option value="2029">2029</option>
+                            <option value="2030">2030</option>
                         </select>
                     </div>
 
